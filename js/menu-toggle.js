eb62c6c0fe50c8fecf16d188ea7802bb54d1e7f3
Dayuhnat <s3864185@rmit.edu.vn>
2024-06-06 14:03:21 +0700

    image-slider

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -9,3 +9,45 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+// $(document).ready(function(){
+//     $('.collab-parter').slick({
+//         centerMode: true,
+//         centerPadding: '60px',
+//         slidesToShow: 2,
+//         responsive: [
+//           {
+//             breakpoint: 768,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '40px',
+//               slidesToShow: 3
+//             }
+//           },
+//           {
+//             breakpoint: 480,
+//             settings: {
+//               arrows: false,
+//               centerMode: true,
+//               centerPadding: '40px',
+//               slidesToShow: 1
+//             }
+//           }
+//         ]
+//       });
+// });
+
+$(document).ready(function(){
+    $('.collab-parter').slick({
+    centerMode: true,
+    // centerPadding: '60px',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:`<button type='button' class='slick-prev slick-arrow'><img src='./pic/vector-left.svg' alt='Next'></button>`,
+    nextArrow:"<button type='button' class='slick-next slick-arrow'><img src='./pic/vector-right.svg' alt='Next'></button>",
+    });
+  });
+

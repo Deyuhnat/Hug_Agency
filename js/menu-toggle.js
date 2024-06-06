fce79545c8b8013336342bbb393e0220b2efec46
Dayuhnat <s3864185@rmit.edu.vn>
2024-06-06 14:55:21 +0700

    fix-Responsive

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.querySelector('.menu-icon');
-    if (menuIcon) {
-        menuIcon.addEventListener('click', function() {
-            const menuList = document.querySelector('.menulist');
-            if (menuList) {
-                menuList.classList.toggle('show');
-            }
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  if (menuIcon) {
+    menuIcon.addEventListener("click", function () {
+      const menuList = document.querySelector(".menulist");
+      if (menuList) {
+        menuList.classList.toggle("show");
+      }
+    });
+  }
 });
 
 // $(document).ready(function(){
@@ -38,16 +38,30 @@ document.addEventListener('DOMContentLoaded', function () {
 //       });
 // });
 
-$(document).ready(function(){
-    $('.collab-parter').slick({
+$(document).ready(function () {
+  $(".collab-parter").slick({
     centerMode: true,
     // centerPadding: '60px',
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
-    prevArrow:`<button type='button' class='slick-prev slick-arrow'><img src='./pic/vector-left.svg' alt='Next'></button>`,
-    nextArrow:"<button type='button' class='slick-next slick-arrow'><img src='./pic/vector-right.svg' alt='Next'></button>",
-    });
+    prevArrow: `<button type='button' class='slick-prev slick-arrow'><img src='./pic/vector-left.svg' alt='Next'></button>`,
+    nextArrow:
+      "<button type='button' class='slick-next slick-arrow'><img src='./pic/vector-right.svg' alt='Next'></button>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
-
+});

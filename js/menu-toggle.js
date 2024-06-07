67dfd58c270240441ac1e51da94129e8d8f4e37c
Dayuhnat <s3864185@rmit.edu.vn>
2024-06-07 11:13:22 +0700

    update-reponsive-slide-4

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -10,34 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-// $(document).ready(function(){
-//     $('.collab-parter').slick({
-//         centerMode: true,
-//         centerPadding: '60px',
-//         slidesToShow: 2,
-//         responsive: [
-//           {
-//             breakpoint: 768,
-//             settings: {
-//               arrows: false,
-//               centerMode: true,
-//               centerPadding: '40px',
-//               slidesToShow: 3
-//             }
-//           },
-//           {
-//             breakpoint: 480,
-//             settings: {
-//               arrows: false,
-//               centerMode: true,
-//               centerPadding: '40px',
-//               slidesToShow: 1
-//             }
-//           }
-//         ]
-//       });
-// });
-
 $(document).ready(function () {
   $(".collab-parter").slick({
     centerMode: true,
@@ -53,7 +25,7 @@ $(document).ready(function () {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
         },
       },
       {
